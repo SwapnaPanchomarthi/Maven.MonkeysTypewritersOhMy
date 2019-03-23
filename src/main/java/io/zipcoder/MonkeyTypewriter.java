47d6f57f4f7f2e5628c98118e345feda6c2a9143
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-03-23 16:08:15 -0400

    Swapna multi threading

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -1,7 +1,12 @@
 package io.zipcoder;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MonkeyTypewriter {
+
+
     public static void main(String[] args) {
+
         String introduction = "It was the best of times,\n" +
                 "it was the blurst of times,\n" +
                 "it was the age of wisdom,\n" +
@@ -24,9 +29,55 @@ public class MonkeyTypewriter {
         // For each Copier(one safe and one unsafe), create and start 5 monkeys copying the introduction to
         // A Tale Of Two Cities.
 
-
         // This wait is here because main is still a thread and we want the main method to print the finished copies
         // after enough time has passed.
+
+
+
+        /*Modify MonkeyTypewriter to create 5 monkeys (threads) using the UnsafeCopier and start them.
+
+        After the sleep, print out the results of the unsafely copied passage.
+
+
+        */
+
+        ReentrantLock lock = new ReentrantLock();
+        UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
+        Thread unsafeMonkey1 = new Thread(unsafeCopier);
+        Thread unsafeMonkey2 = new Thread(unsafeCopier);
+        Thread unsafeMonkey3 = new Thread(unsafeCopier);
+        Thread unsafeMonkey4 = new Thread(unsafeCopier);
+        Thread unsafeMonkey5 = new Thread(unsafeCopier);
+
+        unsafeMonkey1.start();
+        unsafeMonkey2.start();
+        unsafeMonkey3.start();
+        unsafeMonkey4.start();
+        unsafeMonkey5.start();
+
+//            for(int i=0;i<5;i++)
+//            {
+//                unsafeMonkey.start();
+//            }
+
+            SafeCopier safeCopier = new SafeCopier(introduction);
+            Thread safeMonkey1 = new Thread(safeCopier);
+            Thread safeMonkey2 = new Thread(safeCopier);
+            Thread safeMonkey3 = new Thread(safeCopier);
+            Thread safeMonkey4 = new Thread(safeCopier);
+            Thread safeMonkey5 = new Thread(safeCopier);
+
+            safeMonkey1.start();
+            safeMonkey2.start();
+            safeMonkey3.start();
+            safeMonkey4.start();
+            safeMonkey5.start();
+//            for(int i=0;i<5;i++)
+//            {
+//                safeMonkey.start();
+//            }
+
+
         try {
             Thread.sleep(1000);
         } catch(InterruptedException e) {
@@ -34,5 +85,10 @@ public class MonkeyTypewriter {
         }
 
         // Print out the copied versions here.
+        System.out.println("Unsafe Copy:");
+        System.out.println(unsafeCopier.copied+"\n");
+
+        System.out.println("Safe Copy:");
+        System.out.println(safeCopier.copied+"\n");
     }
 }
--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,14 +1,28 @@
 package io.zipcoder;
 
+import java.util.Iterator;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
 public class UnsafeCopier extends Copier {
+ //   private ReentrantLock lock;
 
     public UnsafeCopier(String toCopy) {
         super(toCopy);
+       // this.lock=lock;
     }
 
     public void run() {
+        //Iterator<String> iterator = stringIterator;
+
+
+        while(stringIterator.hasNext())
+        {
+            copied+=stringIterator.next() +' ';
+        }
+
     }
+
 }
